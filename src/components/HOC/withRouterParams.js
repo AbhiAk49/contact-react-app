34b653bc435983bff5352b2c32389f7f018ada87
abhiak49 <abhiak49kumar@gmail.com>
2023-03-24 21:30:56 +0530

    annotations

--- src/components/HOC/withRouterParams.js
+++ src/components/HOC/withRouterParams.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSearchParams, useParams } from "react-router-dom";
 
-//using this Higher Order Component to inject useNavigate from react-router-dom into component passed
+//using this Higher Order Component to inject search (query) and path params
 const withRouterParamsHook = (Component) => {
   return (props) => {
     const params = useParams(); // returns key value pairs of params
